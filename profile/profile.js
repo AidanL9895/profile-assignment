8f4ec28e0aa3a686e376a025797c6491893bd586
AidanL9895 <aleezy9895@gmail.com>
2025-03-27 11:47:50 -0400

    initial commit

---
+++ profile/profile.js
@@ -0,0 +1,8 @@
+const profiler =() => {
+    $("#name").text("Aidan");
+    $("#major").text("Data Science");
+    $("#year").text("2025");
+    $("h4").text("About Me");
+}
+
+$(document).ready(profiler);
